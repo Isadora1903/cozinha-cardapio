891be4409ccdb1e7caa30f5d890427ec90fa3979
Isadora Emilia <isadora.9296887@aluno.mg.gov.br>
2025-10-27 09:02:55 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,36 @@
+async function loginCozinheira(email,senha){
+ 
+}
+
+async function cadastrarCozinheira(email,nome,senha){
+
+}
+
+async function recuperarSenha(email){
+
+}
+
+async function cadastrarCardapio(cardapio){
+
+}
+
+async function alterarCardapio(id,atualizarCardapio){
+
+}
+
+async function excluirCardapio(id){
+    
+}
+
+async function buscarCardapio(id){
+    
+}
+
+function exibirTabelaCardapio(cardapios){
+
+}
+
+async function listarCardapio(){
+
+}
+
